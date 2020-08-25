f9f0f23e8d3bc2290cd91f98746677e412cfaba7
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2020-08-25 00:10:55 -0500

    i add changes on sidebar

--- src/components/aside/Sidebar.vue
+++ src/components/aside/Sidebar.vue
@@ -1,15 +1,15 @@
 <template>
-    <aside id="sidebar" class="w-50">
-        <div id="nav-blog" class="sidebar-item">
-            <h3>Puedes hacer esto</h3>
-            <a href="#" class="btn btn-success">Crear artículo</a>
+    <aside id="sidebar" class="w-25 pb-4 mr-3">
+        <div id="nav-blog" class="sidebar-item mt-4">
+            <p class="title-sidebar">{{ titleSidebar }}</p>
+            <a href="#" class="btn btn-success">{{ btnArticle }}</a>
         </div>
-        <div id="search" class="sidebar-item">
-            <h3>Buscador</h3>
-            <p>Encuentra el artículo que buscas</p>
+        <div id="search" class="sidebar-item mt-5">
+            <p class="title-search">{{ titleSearch }}</p>
+            <p>{{ descriptionSearch }}</p>
             <form action="">
-                <input type="text" name="search" />
-                <input type="submit" name="submit" value="Buscar" class="btn" />
+                <input type="text" name="search" class="w-75" /> <br>
+                <input type="submit" name="submit" value="Buscar" class="btn btn-dark mt-3" />
             </form>
         </div>
     </aside>
@@ -19,11 +19,28 @@
 export default {
     name: 'Sidebar',
     props: {
-        
+        titleSidebar: String,
+        btnArticle: String,
+        titleSearch: String,
+        descriptionSearch: String
     },
 }
 </script>
 
 <style lang="scss">
-
+aside {
+    background-color: #f5f5f5;
+    #nav-blog {
+        .title-sidebar {
+            font-size: 1em;
+            font-weight: 900;
+        }
+    }
+    #search {
+        .title-search {
+            font-size: 1em;
+            font-weight: 900;
+        } 
+    }
+}  
 </style>
