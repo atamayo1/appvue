a61df17ecd9ca81a7b742dfc0c3c85c0214b2469
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2020-08-25 21:57:40 -0500

    i add fixes on mainjs

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import Home from './components/pages/Home.vue'
 import axios from 'axios'
-import ErrorComponent from './components/ErrorComponent.vue'
+import ErrorComponent from './components/pages/ErrorComponent.vue'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
